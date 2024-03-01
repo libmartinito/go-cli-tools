4ad6cb5351ef7fb2e16ed1439cb485657ed5ca90
libmartinito <lib.martinito@pm.me>
2024-03-02 04:13:17 +0800

    fix: update count function to correctly count lines or words based on the countLines parameter

--- wc/main.go
+++ wc/main.go
@@ -19,7 +19,7 @@ func count(r io.Reader, countLines bool) int {
 	scanner := bufio.NewScanner(r)
 
 	if !countLines {
-		scanner.Split(bufio.ScanLines)
+		scanner.Split(bufio.ScanWords)
 	}
 
 	wc := 0
