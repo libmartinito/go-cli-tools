da2f01d57b07caf1d408c353e321584d6df92070
libmartinito <lib.martinito@pm.me>
2024-03-02 04:30:54 +0800

    feat: add new package and types for managing todo lists

---
+++ todo/todo.go
@@ -0,0 +1,19 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type List []item
