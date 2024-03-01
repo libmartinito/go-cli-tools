1e13a41eaa08fe29511df63f7c20158ac4211236
libmartinito <lib.martinito@pm.me>
2024-03-02 04:43:00 +0800

    feat: add Get method to List struct to retrieve data from a file

--- todo/todo.go
+++ todo/todo.go
@@ -61,3 +61,20 @@ func (l *List) Save(filename string) error {
 
 	return os.WriteFile(filename, js, 0644)
 }
+
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, l)
+}
